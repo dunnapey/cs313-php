a11df83723be941e8cc517a073fa86dfbcae0344
dunnapey <pjdunnaway@gmail.com>
2018-09-29 20:39:21 -0600

    Add some Social logos

--- web/a02.php
+++ web/a02.php
@@ -6,7 +6,6 @@ echo
 <head>
     <title>ABOUT ME!</title>
     <link rel='stylesheet' type='text/css' href='a02.css'>
-    <script type='text/javascript' src='https://platform.linkedin.com/badges/js/profile.js' async defer></script>
 </head>
 
 <body>
