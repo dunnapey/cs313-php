a49575d3fc62f59d092df41a721c77057c834326
dunnapey <pjdunnaway@gmail.com>
2018-09-29 19:03:30 -0600

    Edit header

--- web/a02.php
+++ web/a02.php
@@ -5,24 +5,27 @@ echo
 
 <head>
     <title>ABOUT ME!</title>
-    <link rel='stylesheet' type='text/css' href='a02.css'>
+    <link rel='stylesheet' type='text/css' href='a02.cs'>
 </head>
 
 <body>
     <div id='head'>
     <a href='index.html' id='back'><</a><h1>ABOUT ME</h1>
     </div>
+
     <p>
         My name is Peyton Dunnaway and I'm a software engineering major. I'm a Junior and am actively seeking internship opportunities for the coming year. Having dealt mostly with C++ in most of my programming classes, I'm always excited to dive more into Web Development.<br><br>
         I'm originally from Georgia, and lived in Kansas while in High School. I served a mission in Fresno, California, speaking Hmong (from Thailand area).<br><br>
         I got married over the summer and am super excited to begin life with my lovely wife! We love the outdoors, eating good food, binging some Netflix, and going to the Temple!<br><br>
     </p>
+
     <ul>
         <h2>Get to Know Me:<h2>
         <li><a href='https://www.linkedin.com/in/peyton-dunnaway'>LinkedIn</a></li>
         <li><a href='https://github.com/dunnapey/myPortfolio'>GitHub</a></li>
         <li><a href='https://www.facebook.com/peyton.dunnaway'>Facebook</a></li>
     </ul>
+
     <div class='us'>
         <img src='assets/us.jpg' alt='Me and Wife'>
         <img src='assets/us.jpg' alt='Me and Wife'>
