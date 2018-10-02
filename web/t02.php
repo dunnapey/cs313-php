8e751c742ff39b5edf60a124a0d8e567470b0ceb
dunnapey <pjdunnaway@gmail.com>
2018-10-02 16:04:37 -0600

    Team02

---
+++ web/t02.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Team02</title>
+</head>
+
+<body>
+    Name: <input type="text" name="name">
+    Email: <input type="text" name="email">
+    Major: <input type="radio" name="major">
+    Comment: <textarea placeholder="Enter a comment:"></textarea>
+</body>
+</html>
