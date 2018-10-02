ff5d61b1324d319cfb7fb970489c7ef0151da7df
dunnapey <pjdunnaway@gmail.com>
2018-10-02 16:13:19 -0600

    Team02

--- web/t02.php
+++ web/t02.php
@@ -5,10 +5,13 @@
 </head>
 
 <body>
-    Name: <input type="text" name="name"><br>
-    Email: <input type="text" name="email"><br>
+    Name: <input type="text" name="name">
+    <br>
+    Email: <input type="text" name="email">
+    <br>
     Major: <input type="radio" name="major" title="Computer Science"> <input type="radio" name="major" title="Web Design & Development">
-     <input type="radio" name="major" title="Computer Information Technology"> <input type="radio" name="major" title="Computer Engineering"><br>
+     <input type="radio" name="major" title="Computer Information Technology"> <input type="radio" name="major" title="Computer Engineering">
+     <br>
     Comment: <textarea placeholder="Enter a comment:"></textarea>
 </body>
 </html>
