172d7fc49f14fb92b4ac5d58926e1a43e6459455
dunnapey <pjdunnaway@gmail.com>
2018-10-04 16:35:01 -0600

    SESSIONS practice

--- web/cookieload.php
+++ web/cookieload.php
@@ -2,7 +2,6 @@
     session_start();
     $favorite = $_COOKIE["fav_text"];
     $user = $_SESSION["user"];
-    $count = 0;
 
     if (isset($_SESSION["count"])) {
         $_SESSION["count"] = $_SESSION["count"]++;
