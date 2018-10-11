b42f7cf046cb0d4b157b20bfa719b8ffcf8d004d
dunnapey <pjdunnaway@gmail.com>
2018-10-10 19:24:50 -0600

    Added some CSS to a03

--- web/a03.php
+++ web/a03.php
@@ -22,28 +22,63 @@
             <button name="add">Add to Cart</button>
         </div>
         <div class="item">
-            <h3>Ark of the Covenant</h3>
-            <img src="assets/ark1.jpg">
+            <h3>Invisibility Cloak</h3>
+            <img src="assets/cloak.jpg">
             <button name="add">Add to Cart</button>
         </div>
         <div class="item">
-            <h3>Ark of the Covenant</h3>
-            <img src="assets/ark1.jpg">
+            <h3>Old Cursed Diary</h3>
+            <img src="assets/diary.jpg">
             <button name="add">Add to Cart</button>
         </div>
         <div class="item">
-            <h3>Ark of the Covenant</h3>
-            <img src="assets/ark1.jpg">
+            <h3>Best Costume Ever</h3>
+            <img src="assets/dino.jpg">
             <button name="add">Add to Cart</button>
         </div>
         <div class="item">
-            <h3>Ark of the Covenant</h3>
-            <img src="assets/ark1.jpg">
+            <h3>Mobile Radiation Sensor</h3>
+            <img src="assets/geiger.jpg">
             <button name="add">Add to Cart</button>
         </div>
         <div class="item">
-            <h3>Ark of the Covenant</h3>
-            <img src="assets/ark1.jpg">
+            <h3>Evil Crytal Ball</h3>
+            <img src="assets/orb.jpg">
+            <button name="add">Add to Cart</button>
+        </div>
+        <div class="item">
+            <h3>Brass Plates</h3>
+            <img src="assets/plates.jpg">
+            <button name="add">Add to Cart</button>
+        </div>
+        <div class="item">
+            <h3>Shiny Precious</h3>
+            <img src="assets/ring.jpg">
+            <button name="add">Add to Cart</button>
+        </div>
+        <div class="item">
+            <h3>Awesome Laser Sword</h3>
+            <img src="assets/saber.jpg">
+            <button name="add">Add to Cart</button>
+        </div>
+        <div class="item">
+            <h3>Weird Alien Skull</h3>
+            <img src="assets/skull.jpg">
+            <button name="add">Add to Cart</button>
+        </div>
+        <div class="item">
+            <h3>Seer Stone</h3>
+            <img src="assets/stone.jpg">
+            <button name="add">Add to Cart</button>
+        </div>
+        <div class="item">
+            <h3>Ten Commandment Tablets</h3>
+            <img src="assets/tablets.jpg">
+            <button name="add">Add to Cart</button>
+        </div>
+        <div class="item">
+            <h3>Bacon-Flavored Toothpaste</h3>
+            <img src="assets/toothpaste.jpg">
             <button name="add">Add to Cart</button>
         </div>
     </div>
