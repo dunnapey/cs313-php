049ac13af0a3c003beb766e7d988ebee64ae4f84
dunnapey <pjdunnaway@gmail.com>
2018-10-16 15:54:28 -0600

    Team DB

---
+++ db/teamscript.php
@@ -0,0 +1,22 @@
+<?php
+    $user = 'postgres';
+    $pwd = 'hmooBhwJchiM';
+    $db = new PDO('psql:host=localhost;dbname=localtestdb', $user, $pwd);
+
+    // HEROKU VERSION
+    // $dburl = getend('DATABASE_URL');
+    // dbOpts = parse_url($dburl);
+    // $dbHost = $dbOpts['host'];
+    // $dbPort = $dbOpts['port'];
+    // $dbUser = $dbOpts['user'];
+    // $dbPwd = $dbOpts['pass'];
+    // $dbName = ltrim($dbOpts['path'], '/');
+
+    // $db = new PDO("psql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPwd);
+
+    echo "<h1>Scriptures Resources</h1><br>";
+
+    foreach ($db->query('SELECT * FROM scriptures;') as $row) {
+        echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - ' . '\"' . $row['content'] . '\"<br>';
+    }
+?>
