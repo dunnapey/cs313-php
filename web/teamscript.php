9addb859350f7afef374cc901b9167ebe42b1f24
dunnapey <pjdunnaway@gmail.com>
2018-10-16 16:06:22 -0600

    Team DB

--- web/teamscript.php
+++ web/teamscript.php
@@ -8,23 +8,30 @@
     // $user = 'postgres';
     // $pwd = 'hmooBhwJchiM';
     // $db = new PDO('psql:host=localhost;dbname=localtestdb', $user, $pwd);
+    try
+    {
+        $dburl = getend('DATABASE_URL');
+        $dbOpts = parse_url($dburl);
+        $dbHost = $dbOpts['host'];
+        $dbPort = $dbOpts['port'];
+        $dbUser = $dbOpts['user'];
+        $dbPwd = $dbOpts['pass'];
+        $dbName = ltrim($dbOpts['path'], '/');
 
-    HEROKU VERSION
-    $dburl = getend('DATABASE_URL');
-    $dbOpts = parse_url($dburl);
-    $dbHost = $dbOpts['host'];
-    $dbPort = $dbOpts['port'];
-    $dbUser = $dbOpts['user'];
-    $dbPwd = $dbOpts['pass'];
-    $dbName = ltrim($dbOpts['path'], '/');
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPwd);
 
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPwd);
+        echo "<h1>Scriptures Resources</h1><br>";
 
-    echo "<h1>Scriptures Resources</h1><br>";
-
-    foreach ($db->query('SELECT * FROM scriptures;') as $row) {
-        echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - ' . '\"' . $row['content'] . '\"<br>';
+        foreach ($db->query('SELECT * FROM scriptures;') as $row) {
+            echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - ' . '\"' . $row['content'] . '\"<br>';
+        }
+    }
+    catch(PDOException $ex)
+    {
+        echo 'ERROR!: ' . $ex->getMessage();
+        die();
     }
 ?>
+    
 </body>
 </html>
