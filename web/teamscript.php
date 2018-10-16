03c9eb30976cfabade440e6efb03bd47940da878
dunnapey <pjdunnaway@gmail.com>
2018-10-16 16:29:30 -0600

    Team DB

--- web/teamscript.php
+++ web/teamscript.php
@@ -13,7 +13,7 @@
     // $pwd = 'hmooBhwJchiM';
     // $db = new PDO('psql:host=localhost;dbname=localtestdb', $user, $pwd);
 
-    $val = $_POST['submit'];
+    $val = array_values($_POST['submit'])[0];
 
     var_dump($_POST);
     
@@ -31,7 +31,7 @@
 
         echo "<h1>Scriptures Resources</h1><br>";
 
-        foreach ($db->query('SELECT * FROM scriptures WHERE book == $val;') as $row) {
+        foreach ($db->query("SELECT * FROM scriptures WHERE book == $val;") as $row) {
             echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - ' . '\"' . $row['content'] . '\"<br>';
         }
     }
