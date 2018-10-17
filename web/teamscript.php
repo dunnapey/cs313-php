c912f08731025549f734a0ccbe885072330496cf
dunnapey <pjdunnaway@gmail.com>
2018-10-17 11:06:41 -0600

    Team DB

--- web/teamscript.php
+++ web/teamscript.php
@@ -28,7 +28,7 @@
 
         echo "<h1>Scriptures Resources</h1><hr>";
 
-        foreach ($db->query("SELECT book FROM scriptures WHERE book = '$val'") as $row) {
+        foreach ($db->query("SELECT * FROM scriptures WHERE book = '$val'") as $row) {
             echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - ' . "\"" . $row['content'] . "\"<br>";
         }
     }
