734446fbd5c7f385adb1f7d5e2501d8d27b11283
dunnapey <pjdunnaway@gmail.com>
2018-10-23 14:43:34 -0600

    Added Major Project pages

---
+++ web/dating/classes.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Make It Great</title>
+    <link rel="icon" type="image/png" href="assets/favicon2.png">
+    <link rel="stylesheet" type="text/css" href="dating.css">
+</head>
+
+<body>
+    <div id="head"><?php require 'header.php'; ?></div>
+
+    <div id="main">
+        <h2>Classes Available</h2>
+    </div>
+
+    <div id="foot"><?php require 'footer.php'; ?></div>
+</body>
+</html>
---
+++ web/dating/index.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Make It Great</title>
+    <link rel="icon" type="image/png" href="assets/favicon2.png">
+    <link rel="stylesheet" type="text/css" href="dating.css">
+</head>
+
+<body>
+    <div id="head"><?php require 'header.html'; ?></div>
+
+    <div id="main">
+        <h2>Dating Coaching Services</h2>
+    </div>
+
+    <div id="foot"><?php require 'footer.html'; ?></div>
+</body>
+</html>
