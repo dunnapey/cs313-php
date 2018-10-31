5c1a5bd99fa12f65b8dc17004103e4cb64eb0e7f
dunnapey <pjdunnaway@gmail.com>
2018-10-31 17:52:37 -0600

    Debug login

--- db/dating.sql
+++ db/dating.sql
@@ -4,7 +4,7 @@ CREATE TABLE users
     , username VARCHAR(50) UNIQUE NOT NULL
     , fName VARCHAR(50) NOT NULL
     , lName VARCHAR(50) NOT NULL
-    , password VARCHAR(50) NOT NULL
+    , password VARCHAR(255) NOT NULL
     , email VARCHAR(50) UNIQUE NOT NULL
 );
 
