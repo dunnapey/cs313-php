800079121c609dcbd383c2490aec6d8c2de52da0
dunnapey <pjdunnaway@gmail.com>
2018-10-31 00:37:35 -0600

    Debug login

--- web/dating/createuser.php
+++ web/dating/createuser.php
@@ -12,15 +12,14 @@
     $hash = password_hash($pwd, PASSWORD_DEFAULT);
 
     //INSERT NEW USER
-        $insert = $db->prepare("INSERT INTO users (username, fName, lName, password, email) VALUES (:user, :fname, :lname, :pwd, :email);");
-        $insert->bindParam(':user', $user);
-        $insert->bindParam(':fname', $fname);
-        $insert->bindParam(':lname', $lname);
-        $insert->bindParam(':pwd', $hash);
-        $insert->bindParam(':email', $email);
-        $insert->execute();
+    $insert = $db->prepare("INSERT INTO users (username, fName, lName, password, email) VALUES (:user, :fname, :lname, :pwd, :email);");
+    $insert->bindParam(':user', $user);
+    $insert->bindParam(':fname', $fname);
+    $insert->bindParam(':lname', $lname);
+    $insert->bindParam(':pwd', $hash);
+    $insert->bindParam(':email', $email);
+    $insert->execute();
 
-        header("Location: index.php");
-        die();
-    }
+    header("Location: index.php");
+    die();
 ?>
