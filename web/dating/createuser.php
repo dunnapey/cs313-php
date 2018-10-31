b7e075e1de93ecd8392316fcdd6f157f523e29be
dunnapey <pjdunnaway@gmail.com>
2018-10-31 00:29:58 -0600

    Debug login

--- web/dating/createuser.php
+++ web/dating/createuser.php
@@ -2,16 +2,22 @@
     session_start();
     require '../herokudb.php';
 
+    $user = htmlspecialchars($_POST['uname']);
+    $fname = htmlspecialchars($_POST['fname'])
+    $lname = htmlspecialchars($_POST['lname']);
+    $pwd = htmlspecialchars($_POST['pwd']);
+    $email = htmlspecialchars($_POST['email']);
+
     //HASH PASSWORD
-    $hash = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
+    $hash = password_hash($pwd, PASSWORD_DEFAULT);
 
     //INSERT NEW USER
         $insert = $db->prepare("INSERT INTO users (username, fName, lName, password, email) VALUES (:user, :fname, :lname, :pwd, :email);");
-        $insert->bindParam(':user', $_POST['uname']);
-        $insert->bindParam(':fname', $_POST['fname']);
-        $insert->bindParam(':lname', $_POST['lname']);
+        $insert->bindParam(':user', $user);
+        $insert->bindParam(':fname', $fname);
+        $insert->bindParam(':lname', $lname);
         $insert->bindParam(':pwd', $hash);
-        $insert->bindParam(':email', $_POST['email']);
+        $insert->bindParam(':email', $email);
         $insert->execute();
 
         header("Location: index.php");
