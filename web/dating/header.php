4bedf3136cac8550ff1b84229022af9d06b805a0
dunnapey <pjdunnaway@gmail.com>
2018-11-03 14:34:02 -0600

    Edit css

--- web/dating/header.php
+++ web/dating/header.php
@@ -5,24 +5,24 @@
 <div id="head">
     <h1>Make It Great</h1>
     <br>
-    <span id="nav">
-        <a href="index.php" id="home">HOME</a>
-        <a href="classes.php" id="classes">CLASSES</a>
-        <a href="contact.php" id="contact">CONTACT US</a>
-    </span>
+    <ul id="nav">
+        <li><a href="index.php" id="home">HOME</a></li>
+        <li><a href="classes.php" id="classes">CLASSES</a></li>
+        <li><a href="contact.php" id="contact">CONTACT US</a></li>
     <?php
         if ($_SESSION['loggedIn'] == true)
         {
             $user = $_SESSION['username'];
-            echo "<div class='drop'>
+            echo "<li class='drop'>
                     <span>WELCOME $user!</span>
                     <div class='dropContent'>
                         <a href=''>Account</a>
                         <a href='logout.php'>Log Out</a>
                     </div>
-                </div>";
+                </li>";
         } else {
-            echo "<a href='login.php' style='color: white; text-decoration: none; margin-left: 20px;'>SIGN IN</a>";
+            echo "<li><a href='login.php' style='color: white; text-decoration: none; margin-left: 20px;'>SIGN IN</a></li>";
         }
     ?>
+    </ul>
 </div>
